f82d97f3ba73e98223d03da6cf0a2761482c644d
fushilian <fushilian@360.cn>
2019-12-14 11:44:12 +0800

    change image-run-script

--- docker/image_run.sh
+++ docker/image_run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-WORK_DIR=`pwd`
-docker run --name=fsl_reid --runtime=nvidia --net=host --ipc=host -it -v ${WORK_DIR}:/works/deep-person-reid -v /data/cv_hub/datasets:/works/dataset fsl/reid:v1.0 /bin/bash
+WORK_DIR=$(pwd)
+docker run \
+    --name=fsl_reid \
+    --runtime=nvidia \
+    --net=host \
+    --ipc=host \
+    -it \
+    -v ${WORK_DIR}:/works/deep-person-reid \
+    -v /data/cv_hub/datasets:/works/dataset \
+    fsl/reid:v1.0 /bin/bash
